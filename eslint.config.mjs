322c8d0844c4a4c22c64cfd89ee99ceede0b3a1b
unknown <pawel.jaspinski@tellyo.com>
2025-07-05 09:55:59 +0200

    Template cleanup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import eslint from '@eslint/js';
-import vitest from '@vitest/eslint-plugin';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-// This is just an example default config for ESLint.
-// You should change it to your needs following the documentation.
 export default tseslint.config(
   {
     ignores: ['**/build/**', '**/tmp/**', '**/coverage/**'],
@@ -42,24 +39,9 @@ export default tseslint.config(
   },
   {
     files: ['__tests__/**'],
-
-    plugins: {
-      vitest,
-    },
-
-    rules: {
-      ...vitest.configs.recommended.rules,
-    },
-
     settings: {
       vitest: {
         typecheck: true,
-      },
-    },
-
-    languageOptions: {
-      globals: {
-        ...vitest.environments.env.globals,
       },
     },
   },
